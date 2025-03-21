562e55faae502b4cee21f2e2c1e407b68ed18598
w11wo <wilsonwong961@gmail.com>
2025-03-21 16:55:54 +1100

    Split into subtasks; Implement substitutions

--- src/egra_inference.py
+++ src/egra_inference.py
@@ -7,7 +7,6 @@ from tqdm.auto import tqdm
 from transformers import pipeline
 from datasets import load_dataset, Audio
 import numpy as np
-import evaluate
 import Levenshtein
 
 """
@@ -25,15 +24,41 @@ def parse_args():
     parser.add_argument("--model_name", type=str, required=True)
     parser.add_argument("--dataset_name", type=str, required=True)
     parser.add_argument("--split_name", type=str, default="test")
+    parser.add_argument("--subtask", type=str, choices=["syllable", "word"])
     parser.add_argument("--device", type=str, default="cuda")
     parser.add_argument("--audio_column_name", type=str, default="audio")
     parser.add_argument("--label_column_name", type=str, default="phonemes")
     parser.add_argument("--min_length_s", type=float, default=1.0)
     parser.add_argument("--output_dir", type=Path, default=Path("results"))
+    parser.add_argument("--use_substitution_pairs", action="store_true")
     return parser.parse_args()
 
 
-def calculate_error_stats(labels, predictions):
+def phoneme_error_rate(labels, predictions, substitution_pairs=[]):
+    def get_errors(label, prediction, substitution_pairs=[]):
+        _label, _pred = label.split(), prediction.split()
+        n = len(_label)
+        s, d, i = 0, 0, 0
+
+        for tag, i1, i2, j1, j2 in Levenshtein.opcodes(_label, _pred):
+            l = " ".join(_label[i1:i2])
+            p = " ".join(_pred[j1:j2])
+
+            if tag == "replace" and not ((l, p) in substitution_pairs or (p, l) in substitution_pairs):
+                s += 1
+            elif tag == "delete":
+                d += 1
+            elif tag == "insert":
+                i += 1
+
+        return s, d, i, n
+
+    all_errors = [get_errors(label, prediction, substitution_pairs) for label, prediction in zip(labels, predictions)]
+    s, d, i, n = map(sum, zip(*all_errors))
+    return (s + d + i) / n
+
+
+def calculate_error_stats(labels, predictions, substitution_pairs=[]):
     substitutions, insertions, deletions = Counter(), Counter(), Counter()
     errors = Counter([f"{l} -> {p}" for (l, p) in zip(labels, predictions) if l != p])
 
@@ -44,7 +69,7 @@ def calculate_error_stats(labels, predictions):
             l = " ".join(_label[i1:i2])
             p = " ".join(_pred[j1:j2])
 
-            if tag == "replace":
+            if tag == "replace" and not ((l, p) in substitution_pairs or (p, l) in substitution_pairs):
                 substitutions[f"{l} -> {p}"] += 1
             elif tag == "insert":
                 insertions[p] += 1
@@ -72,8 +97,7 @@ def main(args):
 
     dataset = load_dataset(args.dataset_name, split=args.split_name)
     dataset = dataset.cast_column(args.audio_column_name, Audio(sampling_rate=sampling_rate))
-
-    per_metric = evaluate.load("wer")  # each phoneme is treated as a word
+    dataset = dataset.filter(lambda x: x == args.subtask, input_columns="subtask")
 
     def transcribe(datum):
         input_samples = datum[args.audio_column_name]["array"]
@@ -93,20 +117,35 @@ def main(args):
         predictions.append(prediction)
         labels.append(label)
 
-    per = per_metric.compute(references=labels, predictions=predictions)
+    if args.use_substitution_pairs:
+        substitution_pairs = [
+            ("ð", "v"),
+            ("ɣ", "ɠ"),
+            ("ⁿz", "z"),
+            ("x", "h"),
+            ("w", "u"),
+            ("ʃ", "s"),
+            ("θ", "ð"),
+            ("ⁿɗ͡ʒ", "ʄ"),
+        ]
+    else:
+        substitution_pairs = []
+
+    per = phoneme_error_rate(labels, predictions, substitution_pairs)
 
     results = {
         "model": args.model_name,
         "dataset": args.dataset_name,
-        "split": args.split_name,
+        "subtask": args.subtask,
         "per": per,
-        **calculate_error_stats(labels, predictions),
+        "substitution_pairs": substitution_pairs,
+        **calculate_error_stats(labels, predictions, substitution_pairs),
     }
 
     model_name = args.model_name.split("/")[-1]
     dataset_name = args.dataset_name.split("/")[-1]
 
-    with open(f"{args.output_dir}/results_{model_name}_{dataset_name}_{args.split_name}.json", "w") as f:
+    with open(f"{args.output_dir}/results_{model_name}_{dataset_name}_{args.subtask}.json", "w") as f:
         json.dump(results, f, indent=2, ensure_ascii=False)
 
 
